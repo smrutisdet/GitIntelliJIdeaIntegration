cf92067f4a56aa7f9b25047acf1297bd3d597a54
Smrutiranjan <37649900+smrutisdet@users.noreply.github.com>
2023-07-24 11:53:21 +0530

    Fetch -Merge Example commit in remote master

--- src/main/java/qaautomationclasses/Main.java
+++ src/main/java/qaautomationclasses/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Third Commit");
         System.out.println("Fourth Commit");
         System.out.println("Fetch Example");
+        System.out.println("Fetch -Merge Example");
     }
 }
