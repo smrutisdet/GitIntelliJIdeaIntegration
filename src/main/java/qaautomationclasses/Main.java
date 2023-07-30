22007c5ea708ee1eae0dd129919f3ede385f9181
Smrutiranjan <37649900+smrutisdet@users.noreply.github.com>
2023-07-30 15:34:42 +0530

    Update Main.java in login branch

--- src/main/java/qaautomationclasses/Main.java
+++ src/main/java/qaautomationclasses/Main.java
@@ -12,8 +12,9 @@ public class Main {
         System.out.println("Merge conflict demo-2");
         System.out.println("Merge conflict demo-1");
         System.out.println("First commit in user registration branch");
-        for(int i=0;i<14;i++){
-            System.out.println(i);
+      int a=2;
+        while(a<=10){
+            System.out.println(a);
         }
     }
 }
