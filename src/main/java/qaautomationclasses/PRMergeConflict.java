4ca81fbae8f44b599c338de1aa2083b4178fa4c3
smrutisdet <smmohapatra011@gmail.com>
2023-07-30 15:18:38 +0530

    login branch new commit for pr merge conflict

--- src/main/java/qaautomationclasses/PRMergeConflict.java
+++ src/main/java/qaautomationclasses/PRMergeConflict.java
@@ -2,8 +2,10 @@ package qaautomationclasses;
 
 public class PRMergeConflict {
     public static void main(String[] args) {
-        for(int i=0;i<100;i++){
+        int i=0;
+        while(i<100){
             System.out.println(i);
+            i++;
         }
     }
 
